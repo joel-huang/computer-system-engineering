158b61b69ee3242d55770b77ca17954225455aee
joel-huang <huang.joel@hotmail.com>
2018-04-05 21:25:17 +0800

    cp2 still with bug

---
+++ Programming Assignment 2/src/ClientWithoutSecurity.java
@@ -0,0 +1,72 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.net.Socket;
+
+public class ClientWithoutSecurity {
+
+	public static void main(String[] args) {
+
+    	String filename = "rr.txt";
+
+		int numBytes = 0;
+
+		Socket clientSocket = null;
+
+        DataOutputStream toServer = null;
+        DataInputStream fromServer = null;
+
+    	FileInputStream fileInputStream = null;
+        BufferedInputStream bufferedFileInputStream = null;
+
+		long timeStarted = System.nanoTime();
+
+		try {
+
+			System.out.println("Establishing connection to server...");
+
+			// Connect to server and get the input and output streams
+			clientSocket = new Socket("localhost", 4321);
+			toServer = new DataOutputStream(clientSocket.getOutputStream());
+			fromServer = new DataInputStream(clientSocket.getInputStream());
+
+			System.out.println("Sending file...");
+
+			// Send the filename
+			toServer.writeInt(0);
+			toServer.writeInt(filename.getBytes().length);
+			toServer.write(filename.getBytes());
+			toServer.flush();
+
+			// Open the file
+			fileInputStream = new FileInputStream(filename);
+			bufferedFileInputStream = new BufferedInputStream(fileInputStream);
+
+	        byte [] fromFileBuffer = new byte[117];
+
+	        // Send the file
+	        for (boolean fileEnded = false; !fileEnded;) {
+				numBytes = bufferedFileInputStream.read(fromFileBuffer);
+				fileEnded = numBytes < fromFileBuffer.length;
+
+				toServer.writeInt(1);
+				toServer.writeInt(numBytes);
+				toServer.write(fromFileBuffer);
+				toServer.flush();
+			}
+
+	        bufferedFileInputStream.close();
+	        fileInputStream.close();
+
+
+			System.out.println("Closing connection...");
+	        toServer.writeInt(2);
+	        toServer.flush();
+
+		} catch (Exception e) {e.printStackTrace();}
+
+		long timeTaken = System.nanoTime() - timeStarted;
+		System.out.println("Program took: " + timeTaken/1000000.0 + "ms to run");
+	}
+}
---
+++ Programming Assignment 2/src/ServerWithoutSecurity.java
@@ -0,0 +1,68 @@
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerWithoutSecurity {
+
+	public static void main(String[] args) {
+
+		ServerSocket welcomeSocket = null;
+		Socket connectionSocket = null;
+		DataOutputStream toClient = null;
+		DataInputStream fromClient = null;
+
+		FileOutputStream fileOutputStream = null;
+		BufferedOutputStream bufferedFileOutputStream = null;
+
+		try {
+			welcomeSocket = new ServerSocket(4321);
+			connectionSocket = welcomeSocket.accept();
+			fromClient = new DataInputStream(connectionSocket.getInputStream());
+			toClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+			while (!connectionSocket.isClosed()) {
+
+				int packetType = fromClient.readInt();
+
+				// If the packet is for transferring the filename
+				if (packetType == 0) {
+
+					System.out.println("Receiving file...");
+
+					int numBytes = fromClient.readInt();
+					byte [] filename = new byte[numBytes];
+					fromClient.read(filename);
+
+					fileOutputStream = new FileOutputStream("recv/"+new String(filename, 0, numBytes));
+					bufferedFileOutputStream = new BufferedOutputStream(fileOutputStream);
+
+				// If the packet is for transferring a chunk of the file
+				} else if (packetType == 1) {
+
+					int numBytes = fromClient.readInt();
+					byte [] block = new byte[numBytes];
+					fromClient.read(block);
+
+					if (numBytes > 0)
+						bufferedFileOutputStream.write(block, 0, numBytes);
+
+				} else if (packetType == 2) {
+
+					System.out.println("Closing connection...");
+
+					if (bufferedFileOutputStream != null) bufferedFileOutputStream.close();
+					if (bufferedFileOutputStream != null) fileOutputStream.close();
+					fromClient.close();
+					toClient.close();
+					connectionSocket.close();
+				}
+
+			}
+		} catch (Exception e) {e.printStackTrace();}
+
+	}
+
+}
